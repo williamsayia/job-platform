40a41b5be437adb7bf06f87ecf3672134e6d1fa6
Willis <163108588+williamsayia@users.noreply.github.com>
2025-05-25 12:05:05 +0300

    changed the endpoint

--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -21,7 +21,7 @@ const deleteJob = async () => {
   try {
     const confirm = window.confirm('Are you sure you want to delete this job?');
     if (confirm) {
-      await axios.delete(`/api/jobs/${jobId}`);
+      await axios.delete(`${import.meta.env.VITE_API_BASE_URL}/jobs/${jobId}`);
       toast.success('Job Deleted Successfully');
       router.push('/jobs');
     }
@@ -33,7 +33,7 @@ const deleteJob = async () => {
 
 onMounted(async () => {
   try {
-    const response = await axios.get(`/api/jobs/${jobId}`);
+    const response = await axios.get(`${import.meta.env.VITE_API_BASE_URL}/jobs/${jobId}`);
     state.job = response.data;
   } catch (error) {
     console.error('Error fetching job', error);
