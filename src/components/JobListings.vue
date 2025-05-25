c66851ba38cad702de313a37eee1d14b70673852
Willis <163108588+williamsayia@users.noreply.github.com>
2025-05-25 11:16:50 +0300

    updted api endpoint

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -20,7 +20,7 @@ const state = reactive({
 
 onMounted(async () => {
   try {
-    const response = await axios.get('/api/jobs');
+    const response = await axios.get('https://job-backend-qo34.onrender.com/jobs');
     state.jobs = response.data;
   } catch (error) {
     console.error('Error fetching jobs', error);
